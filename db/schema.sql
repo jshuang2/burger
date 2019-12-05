d9a4d7b9055f43973092c13029641d2304e80b4d
Jeff Huang <jeffshuang2@gmail.com>
2019-12-05 13:45:49 -0800

    Created mysql database and added initial data to the table.

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+### SCHEMA
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(300) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
+
+
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheeseburger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Sushi Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Whiskey BBQ Burger");
+
+
+
