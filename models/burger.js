f8d20d4ac18918a867966983dadbe7ea72a77e81
Jeff Huang <jeffshuang2@gmail.com>
2019-12-09 12:10:32 -0800

    Created burger specific orm methods for GET, POST, and UPDATE. Created a 'controllers' folder to hold the app's controllers portion.

--- models/burger.js
+++ models/burger.js
@@ -7,6 +7,17 @@ var burger = {
             cb(res);
         })
     },
-    
+    insert: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+          cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+      }
 
-}
+};
+
+module.exports = burger;
